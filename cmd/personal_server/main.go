94677053431d69f1f8a8e50a271d7eeb9fb66301
DannyahIA <danieltavares.mata@gmail.com>
2024-11-04 15:13:12 -0300

    updates com database

--- cmd/personal_server/main.go
+++ cmd/personal_server/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/DannyahIA/personal-server/internal/database"
 	"github.com/DannyahIA/personal-server/internal/router"
 )
 
 func main() {
 	r := router.SetupRoutes()
 
+	err := database.ConnectDatabase()
+	if err != nil {
+		log.Println(err)
+	}
+
 	fmt.Println("Server is running on port 8080")
-	err := r.Run(":8080")
+	err = r.Run(":8080")
 	if err != nil {
 		panic(err)
 	}
--- internal/router/router.go
+++ internal/router/router.go
@@ -21,14 +21,56 @@ func SetupRoutes() *gin.Engine {
 
 	file_manager := r.Group("/file-manager")
 	{
-		file_manager.GET("/list-root-folders", controller.ListRootFoldersHandler)
-		file_manager.POST("/list-files", controller.ListFilesHandler)
-		file_manager.POST("search-files", controller.SearchFileHandler)
-		file_manager.POST("/create-folder/:folder", controller.CreateFolderHandler)
-		file_manager.POST("/upload-file", controller.UploadFileHandler)
-		file_manager.DELETE("/delete-file", controller.DeleteFileHandler)
-		file_manager.POST("/download-file", controller.DownloadFileHandler)
-		file_manager.PUT("/rename-file", controller.RenameFileHandler)
+		itemRoute := file_manager.Group("/item")
+		{
+			itemRoute.GET("/list", controller.ListItemsHandler)
+			itemRoute.POST("/new-item", controller.CreateItemHandler)
+			itemRoute.DELETE("/", controller.DeleteItemHandler)
+			itemRoute.PATCH("/", controller.UpdateItemHandler)
+			itemRoute.GET("/search", controller.SearchItemHandler)
+			itemRoute.GET("/download", controller.DownloadItemHandler)
+			itemRoute.POST("/upload", controller.UploadItemHandler)
+		}
+
+		profileRoute := file_manager.Group("/profile")
+		{
+			profileRoute.GET("/login", controller.LoginHandler)
+			profileRoute.POST("/register", controller.RegisterHandler)
+			profileRoute.PATCH("/update", controller.UpdateProfileHandler)
+			profileRoute.DELETE("/delete", controller.DeleteProfileHandler)
+			profileRoute.GET("/forgot-password", controller.ForgotPasswordHandler)
+		}
+
+		backupRoute := file_manager.Group("/backup")
+		{
+			backupRoute.GET("/list", controller.ListBackupItemsHandler)
+			backupRoute.GET("/", controller.GetBackupItemHandler)
+			backupRoute.POST("/new-backup", controller.CreateNewBackupHandler)
+			backupRoute.POST("/restore-backup", controller.RestoreBackupHandler)
+			backupRoute.DELETE("/", controller.DeleteBackupHandler)
+		}
+
+		starredRoute := file_manager.Group("/starred")
+		{
+			starredRoute.GET("/list", controller.ListStarredItemsHandler)
+			starredRoute.POST("/", controller.StarItemHandler)
+			starredRoute.DELETE("/", controller.UnstarItemHandler)
+		}
+
+		recentRoute := file_manager.Group("/recent")
+		{
+			recentRoute.GET("/list", controller.ListRecentItemsHandler)
+			recentRoute.POST("/", controller.AddRecentItemHandler)
+			recentRoute.DELETE("/", controller.DeleteRecentItemHandler)
+		}
+
+		trashRoute := file_manager.Group("/trash")
+		{
+			trashRoute.GET("/list", controller.ListTrashItemsHandler)
+			trashRoute.POST("/", controller.MoveToTrashHandler)
+			trashRoute.POST("/restore", controller.RestoreTrashItemHandler)
+			trashRoute.DELETE("/", controller.DeleteTrashItemHandler)
+		}
 	}
 	return r
 }
